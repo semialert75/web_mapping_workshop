644dde36a5ca6598dc8471f9e048d1fad8fe6790
semialert75 <kellyquick75@gmail.com>
2016-05-02 17:17:12 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -118,7 +118,7 @@ function getDirections(frm, to){
               		"stroke": "#ed23f1",
               		"stroke-opacity": 0.8,
               		"stroke-width": 8
-              }
+              })
         })
     })
 }
