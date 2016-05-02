d7a3e385e31616fda82d86136d9418d88993cc97
semialert75 <kellyquick75@gmail.com>
2016-05-02 17:37:07 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -122,6 +122,13 @@ function getDirections(frm, to){
               		"stroke-width": 8
               }
         })
+        
+        $('#directions').fadeIn(400, function (){
+          	var summary = data.trip.summary
+        	$('#summary').empty();
+          	$('#distance').text((Math.round(summary.length * 100) / 100) + data.trip.units;
+          	$('#time').text((Math.round(summary.time / 60 * 100 / 100) + 'min');
+        })
     })
 }
 
