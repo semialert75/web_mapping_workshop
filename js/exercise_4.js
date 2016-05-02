3c68fff56aece0f69dd8fb781165475fa69983f1
semialert75 <kellyquick75@gmail.com>
2016-05-02 16:34:49 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -78,7 +78,7 @@ map.on('locationfound', function(e){
     	type: 'Feature',
     	geometry: {
         	type: 'Point',
-        	coordinates: [e.latlng.lng, e.latlong.lat ]
+        	coordinates: [e.latlng.lng, e.latlng.lat ]
           },
       	properties: {
           	"title": "Here I am!",
