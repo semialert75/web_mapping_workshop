d82ad1990befafb097b923f979dd81655081de62
semialert75 <kellyquick75@gmail.com>
2016-05-02 14:35:24 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'semialert75.019hbg7a';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1Ijoic2VtaWFsZXJ0NzUiLCJhIjoiY2lub29mYWEwMHp0eHVhbTNtbzczMHV4OCJ9.55NNakfh4BHqxzZoPU8RFg';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
